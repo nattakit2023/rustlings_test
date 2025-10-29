ad871d235c4974624bb5ff08c50fff28537bdc58
nattakit <nattakit@NATTAKIT>
2025-10-29 17:02:27 +0700

    update rustlings tests 20

---
+++ exercises/01_variables/variables2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x = 42;
+
+    if 10 == x {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
